4f380008bba7d0bfb9bac58fd01925925a1e5a11
Fathi <fathi.chabane@gmail.com>
2020-10-25 19:31:38 +0100

    Adding Entity Badge.

---
+++ machineAcafe/Machine.Core/Entities/Badge.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Machine.Core.Entities
+{
+    public class Badge
+    {
+        public int Id { get; set; }
+        public string Serial { get; set; }
+        public string Name { get; set; }
+
+    }
+}
